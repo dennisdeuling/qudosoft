224a9578a0dbe5db2bc733b564cb12ff72a9133d
dennis deuling <dennis.deuling@icloud.com>
2022-01-21 14:31:18 +0100

    created backend

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const PORT = 8000
+require('dotenv').config();
+const PORT = process.env.REACT_APP_BACKEND_PORT;
+const express = require('express');
+const csv = require('csvtojson');
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+	const csvFilePath = './output.csv';
+
+	csv()
+		.fromFile(csvFilePath)
+		.then(data => {
+			res.json(data);
+		});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
